4b00a44f4233a6029dc065340a8d452b6f38a9be
Alex <derdef360@gmail.com>
2024-02-10 18:02:14 +0200

    sucsessfull adding files

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        string userInput1 = Console.ReadLine();
+        double number1 = Convert.ToDouble(userInput1);
+
+        Console.Write("Введіть друге число: ");
+        string userInput2 = Console.ReadLine();
+        double number2 = Convert.ToDouble(userInput2);
+
+        double sum = number1 + number2;
+
+        Console.WriteLine("Сума цих двох чисел: " + sum);
+        Console.ReadLine();
+    }
+}
